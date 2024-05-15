8df530c60d02ea30413b2cc7a5fe349d38a98795
rachael baikey <rachaelacb@gmail.com>
2024-05-15 20:31:49 +0000

    secret key

--- ridingreader/settings.py
+++ ridingreader/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q$ak6@i#k!w254nhc0j5*if82zjf^8k3tj@z$td(wj-@+0gja#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
