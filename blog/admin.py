4aa3473ed924707f84fb960d73b64ca94f9de55a
rachael baikey <rachaelacb@gmail.com>
2024-05-15 23:02:31 +0000

    Add comment model to admin.py

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
 from .models import Post
-#Comment
+from .models import Comment
 
 # Register your models here.
 
 admin.site.register(Post)
-#admin.site.register(Comment)
+admin.site.register(Comment)
