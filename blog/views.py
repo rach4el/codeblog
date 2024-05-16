6ba498713ecb443c49ca89a6639b7004a0426536
rachael baikey <rachaelacb@gmail.com>
2024-05-16 23:19:36 +0000

    enable serving of static files

--- blog/views.py
+++ blog/views.py
@@ -8,5 +8,5 @@ from .models import Post
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
     template_name = "blog/index.html"
-    paginate_by = 6
+    paginate_by = 2
 
