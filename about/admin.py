a158019fe2075197aa59c3628ac26b233bf9ba74
rachael baikey <rachaelacb@gmail.com>
2024-05-21 17:18:03 +0000

    Added media query to about page and styling

--- about/admin.py
+++ about/admin.py
@@ -1,10 +1,16 @@
 from django.contrib import admin
-from .models import About
+from .models import About, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
 
 @admin.register(About)
 class AboutAdmin(SummernoteModelAdmin):
-
     summernote_fields = ('content',)
+
+# admin.ModelAdmin is the standard way of registering
+#       our model with the admin panel. Customisation format below.
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
--- about/models.py
+++ about/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 
+# Models below here.
 
-# Create your models here.
-
+# About page description model
 
 class About(models.Model):
     title = models.CharField(max_length=200)
@@ -11,3 +11,14 @@ class About(models.Model):
 
     def __str__(self):
         return self.title
+
+# Collaboration form model
+
+class CollaborateRequest(models.Model):
+    name = models.CharField(max_length=200)
+    email = models.EmailField()
+    message = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"Collaboration request from {self.name}"
