43e456adcf2420c17ce9e60f79e5644886738aaa
rachael baikey <rachaelacb@gmail.com>
2024-05-22 20:57:54 +0000

    Add image upload functionality

--- about/models.py
+++ about/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from cloudinary.models import CloudinaryField
 
 # Models below here.
 
@@ -6,6 +7,7 @@ from django.db import models
 
 class About(models.Model):
     title = models.CharField(max_length=200)
+    profile_image = CloudinaryField('image', default='placeholder')
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
 
