dda3f2595d2a61174e22dedf2c0ceb682156d57a
rachael baikey <rachaelacb@gmail.com>
2024-05-22 23:43:17 +0000

    correct post detail line 17 syntax error correction

--- about/urls.py
+++ about/urls.py
@@ -4,3 +4,16 @@ from django.urls import path
 urlpatterns = [
     path('', views.about_me, name='about'),
 ]
+
+import cloudinary
+import cloudinary.uploader
+from cloudinary.utils import cloudinary_url
+
+# Configuration       
+cloudinary.config( 
+    cloud_name = "dilhkd69z", 
+    api_key = "377163352922742", 
+    api_secret = "<your_api_secret>", # Click 'View Credentials' below to copy your API secret
+    secure=True
+)
+
