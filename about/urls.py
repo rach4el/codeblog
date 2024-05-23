3ed776983087aceb6aabbbb2553f3efe69ff0768
rachael baikey <rachaelacb@gmail.com>
2024-05-23 13:47:51 +0000

    Corrected image upload error fixed

--- about/urls.py
+++ about/urls.py
@@ -5,15 +5,4 @@ urlpatterns = [
     path('', views.about_me, name='about'),
 ]
 
-import cloudinary
-import cloudinary.uploader
-from cloudinary.utils import cloudinary_url
-
-# Configuration       
-cloudinary.config( 
-    cloud_name = "dilhkd69z", 
-    api_key = "377163352922742", 
-    api_secret = "<your_api_secret>", # Click 'View Credentials' below to copy your API secret
-    secure=True
-)
 
--- ridingreader/settings.py
+++ ridingreader/settings.py
@@ -20,12 +20,6 @@ if os.path.isfile('env.py'):
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 
-# Cloudinary imports
-
-import cloudinary
-import cloudinary.uploader
-import cloudinary.api
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
